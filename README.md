fe9166b62b28d842d7899703902a92a0df3aab44
Tamas Kalman <ktamas77@gmail.com>
2018-01-03 00:01:52 -0800

    Initial Stats

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# minestat
-MineStat
+# MineStat
+
+Simple statistics for your mining operations.
+
+Currently supports Ethos StatsPanel & Ethermine.
+
+To install:
+* Please see `config.yaml.sample` for configuration details.
+* Run `composer update` to install required libraries
+
+Requirements:
+* Webserver (Apache/Nginx, etc) + PHP
+
+Run local / dev:
+* `php -S localhost:8080 -t web`
+ 
---
+++ web/index.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$configFile = __DIR__ . '/../config.yaml';
+
+if (!is_file($configFile)) {
+    echo "Please create a configuration file - see details in config.yaml.sample file.";
+    die();
+}
+
+$configData = file_get_contents($configFile);
+$config = yaml_parse($configData);
+
+$app = new \Slim\App();
+$container = $app->getContainer();
+$container['view'] = function ($container) {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates/');
+};
+
+$app->get('/', function ($request, $response, $args) {
+    global $config;
+    $statData = getStatData($config);
+    return $this->view->render($response, 'index.html', $statData);
+});
+
+// Run app
+$app->run();
+
+// --- get details ---
+function getStatData($config) {
+    $statData = array();
+
+    // -- get ethos data ---
+    $ethosPanelId = $config['ethos']['panel'];
+    $ethosApi = "http://$ethosPanelId.ethosdistro.com/?json=yes";
+    $ethosData = json_decode(file_get_contents($ethosApi), true);
+    $excludedRigs = $config['ethos']['excluded_rigs'];
+    $rigs = $ethosData['rigs'];
+    $totalGpuNum = 0;
+    $totalMinersNum = 0;
+    $totalHash = 0;
+    $maxTemp = 0;
+    foreach ($rigs as $rigId => $rig) {
+        if (!in_array($rigId, $excludedRigs)) {
+            $gpuNum = $rig['gpus'];
+            $miners = $rig['miner_instance'];
+            $hash = $rig['hash'];
+            $maxTemp = max(explode(' ', $rig['temp'] . ' ' . $maxTemp));
+
+            $totalGpuNum += $gpuNum;
+            $totalMinersNum += $miners;
+            $totalHash += $hash;
+        }
+    }
+    $statData['total_gpu'] = $totalGpuNum;
+    $statData['total_miner'] = $totalMinersNum;
+    $statData['total_hash'] = $totalHash;
+    $statData['max_temp'] = $maxTemp;
+
+    // -- get ethermine data --
+    $ethermineWallets = $config['pools']['ethermine'];
+    $usdPerMin = 0;
+    $unpaid = array();
+    $minPayout = array();
+    foreach ($ethermineWallets as $ethermineWallet)
+    {
+        $minerStatsUrl = "https://api.ethermine.org/miner/$ethermineWallet/currentStats";
+        $etherMineData = json_decode(file_get_contents($minerStatsUrl), true);
+        $data = $etherMineData['data'];
+        if ($data['activeWorkers'] !== null) {
+            $usdPerMin += $data['usdPerMin'];
+            $unpaid[$ethermineWallet] = $data['unpaid'];
+            $minerSettingsURL = "https://api.ethermine.org/miner/$ethermineWallet/settings";
+            $ethermineSettings = json_decode(file_get_contents($minerSettingsURL), true);
+            $minPayout[$ethermineWallet] = $ethermineSettings['data']['minPayout'];
+        }
+    }
+    $usdPerMonth = $usdPerMin * 60 * 24 * 30;
+    $statData['usd_per_month'] = $usdPerMonth;
+    $statData['eth_unpaid'] = $unpaid;
+    $statData['eth_min_payout'] = $minPayout;
+
+    $poolStatsUrl = "https://api.ethermine.org/poolStats";
+    $poolStatsData = json_decode(file_get_contents($poolStatsUrl), true);
+    $ethPrice = $poolStatsData['data']['price']['usd'];
+    $statData['eth_price'] = $ethPrice;
+
+    return $statData;
+}
